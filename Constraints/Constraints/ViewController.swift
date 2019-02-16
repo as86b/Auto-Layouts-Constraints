02d510e4ef2ac24257a936dfe0177360e1f7f207
Austin Sizemore <austinsizemore@austins-mbp.lan>
2019-02-15 22:14:02 -0600

    Made a new project

---
+++ Constraints/Constraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Austin Sizemore on 2/15/19.
+//  Copyright © 2019 Austin Sizemore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
